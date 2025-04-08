baee02f238a5bcae929dca8f4e8ffa4c874e9a54
Silver <71855839+shockingfargroundx@users.noreply.github.com>
2025-04-08 07:29:51 -0700

    Delete cmake-build-debug directory

--- cmake-build-debug/thirdparty/raylib/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/DEV/LearnCMake2/RaylibplusTMX/thirdparty/raylib/src
-# Build directory: C:/DEV/LearnCMake2/RaylibplusTMX/cmake-build-debug/thirdparty/raylib/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("external/glfw")
